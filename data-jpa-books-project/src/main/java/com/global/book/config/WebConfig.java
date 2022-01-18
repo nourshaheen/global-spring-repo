7fc458b08596ea61f1b802e0bf31ceb693937097
Nour Shaheen <mailto:nshaheen@youxel.com>
2022-01-18 21:29:52 +0200

    change bundle

--- data-jpa-books-project/src/main/java/com/global/book/config/WebConfig.java
+++ data-jpa-books-project/src/main/java/com/global/book/config/WebConfig.java
@@ -1,22 +1,23 @@
 package com.global.book.config;
 
+import java.util.Locale;
+
 import javax.sql.DataSource;
 
 import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
@@ -49,25 +50,32 @@ public class WebConfig implements WebMvcConfigurer {
 		return bean;
 	}
 	
+	
+//	 @Bean
+//	  public LocaleResolver localeResolver() {
+//	      SessionLocaleResolver slr = new SessionLocaleResolver();
+//	      slr.setDefaultLocale(Locale.US);
+//	      slr.setLocaleAttributeName("session.current.locale");
+//	      slr.setTimeZoneAttributeName("session.current.timezone");
+//	      return slr;
+//	  }
+
+	/**
+	 * will switch to a new locale based on the value of the language parameter
+	 *  appended to an HTTP request URL. (url?language=en)
+	 * @return
+	 */
 //	@Bean
-//	public LocaleResolver localeResolver() {
-//	    SessionLocaleResolver slr = new SessionLocaleResolver();
-//	    slr.setDefaultLocale(Locale.US);
-//	    return slr;
+//	public LocaleChangeInterceptor localeChangeInterceptor() {
+//	    LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+//	    lci.setParamName("lang"); 
+//	    return lci;
 //	}
-
-	//LocaleChangeInterceptor
-	@Bean
-	public LocaleChangeInterceptor localeChangeInterceptor() {
-	    LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
-	    lci.setParamName("lang");
-	    return lci;
-	}
-
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-	    registry.addInterceptor(localeChangeInterceptor());
-	}
+//
+//	@Override
+//	public void addInterceptors(InterceptorRegistry registry) {
+//	    registry.addInterceptor(localeChangeInterceptor());
+//	}
 	
 	
 }
